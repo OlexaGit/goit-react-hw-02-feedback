abc1ec01b6d222e6cf01400d5c7a4508158e0b61
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-05-28 13:52:25 +0300

    add key

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,14 +1,18 @@
+import PropTypes from 'prop-types';
 // import css from './Feedback.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <div>
-      {Object.keys(options).map(item => (
-        <button type="button" onClick={() => onLeaveFeedback(item)}>
+      {Object.keys(options).map((item, index) => (
+        <button key={index} type="button" onClick={() => onLeaveFeedback(item)}>
           {item}
         </button>
       ))}
     </div>
   );
+};
+FeedbackOptions.propTypes = {
+  key: PropTypes.number,
 };
 export default FeedbackOptions;
